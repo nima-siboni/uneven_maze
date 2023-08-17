8b2228e9612b363fdb2ee9120bf338eef1d9f91d
Nima H. Siboni <nima.siboni@gmail.com>
2023-08-17 02:21:36 +0200

    configuring the linters

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
+"""Setup file for uneven_maze package."""
 from __future__ import annotations
 
-from setuptools import find_packages
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name="uneven_maze",
